4a3d75db05f58901835f841203513332da22dc2f
JSanjayram <164892498+JSanjayram@users.noreply.github.com>
2025-02-18 16:57:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,6 +21,9 @@ async def get_audio_uri(video_id: str):
                 'preferredquality': '192',
             }],
             'noplaylist': True,
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+            }
         }
 
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
