9bd0e1fdac43a865ac7b362cb80c0b69ad5c7a43
JSanjayram <164892498+JSanjayram@users.noreply.github.com>
2025-02-20 18:11:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_audio_uri(video_id):
     'http_headers': {
         'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
-}
+   }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         try:
