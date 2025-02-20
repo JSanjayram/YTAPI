e0c30e16d108592ef44ba6c1ecfd2ec7e1176e70
JSanjayram <164892498+JSanjayram@users.noreply.github.com>
2025-02-20 18:15:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,14 +15,13 @@ def get_audio_uri(video_id):
         'User -Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
    }
-
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        try:
-            info = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
-            audio_url = info['url']
-            return jsonify({'audio_url': audio_url})
-        except Exception as e:
-            return jsonify({'error': str(e)}), 500
+   with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+      try:
+         info = ydl.extract_info(f'https://www.youtube.com/watch?v={video_id}', download=False)
+         audio_url = info['url']
+         return jsonify({'audio_url': audio_url})
+      except Exception as e:
+         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
